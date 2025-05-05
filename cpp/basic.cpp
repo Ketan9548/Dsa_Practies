3ca4c7684b3f33e4cec9583f5519e2f3ca586b6e
Ketan9548 <kc918062@gmail.com>
2025-05-05 10:39:59 +0530

    DSA Practies

---
+++ cpp/basic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 43;
+    int b = 32;
+    string s = "kapil chauhan";
+    long long int val = 312143232432;
+    cout<<val;
+    // char p = 'k';
+    // cout << p;
+    // cout<<endl;
+    // cout << s << endl;
+
+    cout << a << " " << b << endl; 
+    // cout<<"Hello World"<<endl;
+
+    return 0;
+}
---
+++ cpp/driving_licence_elegibale.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout << "enter you age: " << endl;
+    cin >> age;
+    if (age < 18)
+    {
+        cout << "You are not eligible for driving licence" << endl;
+    }
+    else if (age >= 18 && age <= 21)
+    {
+        cout << "You are eligible for Learning licence" << endl;
+    }
+    else
+    {
+        cout << "You are eligible for Driving licence" << endl;
+    }
+
+    return 0;
+}
